4679a5794f7b94eb94a68e2eb91acc048af3782f
Adelys Belen <adalbeca@gmail.com>
2025-02-15 15:45:19 +0100

    initial commit

---
+++ build/client/assets/index-Jmzab6WR.js
@@ -0,0 +1,1 @@
+import{w as n}from"./with-props-C5Yeyvd2.js";import{o}from"./chunk-IR6S3I6Y-0ihj1Wgf.js";const r=()=>o.jsx("div",{children:o.jsx("h3",{children:"Login!"})}),s=n(r);export{s as default};
---
+++ src/navigation/public.routes.tsx
@@ -0,0 +1,11 @@
+import {
+  index,
+  layout
+} from "@react-router/dev/routes";
+
+export default [
+  layout("pages/layout/Public/index.tsx", [
+    index("pages/public/Login/index.tsx"),
+  ]),
+
+]; 
