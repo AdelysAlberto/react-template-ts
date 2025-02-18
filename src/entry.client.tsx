5d20377cd9ff44b0abbdbfc649ddaaf7036dd906
Adelys Belen <adalbeca@gmail.com>
2025-02-18 19:43:18 +0100

    chore: add button component

--- src/entry.client.tsx
+++ src/entry.client.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import {HydratedRouter} from "react-router/dom";
 
-import "./index.css";
-
 ReactDOM.hydrateRoot(
   document,
   <React.StrictMode>
--- src/pages/public/Login/index.tsx
+++ src/pages/public/Login/index.tsx
@@ -1,8 +1,34 @@
+import BaseButton from "../../../components/BaseButton";
+
 const LoginPage = () => {
 
   return (
     <div>
-      <h3>Login!</h3>
+      <h1>Login</h1>
+      <BaseButton
+        title="Login"
+        id="submit-login"
+        onClick={() => console.log("login")}
+        variant="primary"
+      />
+      <BaseButton
+        title="secondary"
+        id="submit-login"
+        onClick={() => console.log("login")}
+        variant="secondary"
+      />
+      <BaseButton
+        title="danger"
+        id="submit-login"
+        onClick={() => console.log("login")}
+        variant="danger"
+      />
+      <BaseButton
+        title="disabled"
+        id="submit-login"
+        onClick={() => console.log("login")}
+        variant="disabled"
+      />
     </div>
   );
 };
